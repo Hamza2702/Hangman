ef721055cfec24c80279d767fb78094931cbf232
Hamza <HamzaKhan2702@outlook.com>
2024-06-05 22:24:23 +0100

    Add project files.

---
+++ Hangman/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Hangman
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(397, 431);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(141, 54);
+            this.btnSubmit.TabIndex = 0;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnSubmit);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(544, 507);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(568, 531);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Hangman/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hangman
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            string[] words = { "tutorial", "coding", "programmer", "csharp" };
+
+            Random random = new Random();
+            string selectedWord = words[random.Next(words.Length)];
+            char[] wordToGuess = selectedWord.ToCharArray();
+            char[] guessedLetters = new char[wordToGuess.Length];
+
+            for (int i = 0; i < guessedLetters.Length; i++)
+            {
+                guessedLetters[i] = '_';
+
+            }
+
+            //code of the Game
+
+            int attemptsLeft = 10;
+
+            while (attemptsLeft > 0)
+            {
+                Console.WriteLine("Current word: " + new string(guessedLetters));
+
+                Console.WriteLine("Guess a letter: ");
+                char guess = char.ToLower(Console.ReadKey().KeyChar);
+                Console.WriteLine();
+
+                bool correctGuess = false;
+
+                for (int i = 0; i < wordToGuess.Length; i++)
+                {
+                    if (wordToGuess[i] == guess)
+                    {
+                        guessedLetters[i] = guess;
+                        correctGuess = true;
+                    }
+                }
+
+                if (!correctGuess)
+                {
+                    attemptsLeft--;
+                    Console.WriteLine("Incorrect! Attempts left: " + attemptsLeft);
+                }
+                if (new string(guessedLetters) == selectedWord)
+                {
+                    Console.WriteLine("Congratulations! You guessed the word: " + selectedWord);
+                    Console.ReadLine();
+                    break;
+                }
+            }
+
+            // Show the outcome of the game
+
+            if (attemptsLeft == 0)
+            {
+                Console.WriteLine("Sorry, you have ran out of attempts.- The word was: " + selectedWord);
+                Console.ReadLine();
+            }
+
+            Console.WriteLine("Thanks for playing Hangman!");
+            Console.ReadLine();
+        }
+    }
+}
